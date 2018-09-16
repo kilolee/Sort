09823ebc9c84ba27309c2f4861763be2fef412c3
knightlylee <642026443@qq.com>
2018-09-16 22:15:22 +0800

    快速排序-新

---
+++ src/insertion/InsertionSort.java
@@ -0,0 +1,32 @@
+package insertion;
+
+/**
+ * Created by kilo on 2018/9/16.
+ * 插入排序
+ */
+public class InsertionSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            //寻找元素arr[i]合适的插入位置
+            for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 5, 1, 8, 7, 9, 6, 2, 3, 0, 1};
+        sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+    }
+
+}
