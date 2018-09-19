87d560baadc9ecbbf4e11f5bab55917784f2e7aa
knightlylee <642026443@qq.com>
2018-09-19 10:30:33 +0800

    归并排序

--- src/merge/MergeSort.java
+++ src/merge/MergeSort.java
@@ -19,24 +19,32 @@ public class MergeSort {
     private static void merge(int[] arr, int l, int mid, int r) {
         int[] aux = Arrays.copyOfRange(arr, l, r + 1);
 
+        // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
         int i = l, j = mid + 1;
         for (int k = l; k <= r; k++) {
-            if (i > mid) {
+            if (i > mid) { // 如果左半部分元素已经全部处理完毕
                 arr[k] = aux[j - l];
                 j++;
-            } else if (j > r) {
+            } else if (j > r) { // 如果右半部分元素已经全部处理完毕
                 arr[k] = aux[i - l];
                 i++;
-            } else if (aux[i - l] < aux[j - l]) {
+            } else if (aux[i - l] < aux[j - l]) { // 左半部分所指元素 < 右半部分所指元素
                 arr[k] = aux[i - l];
                 i++;
-            } else {
+            } else { // 左半部分所指元素 >= 右半部分所指元素
                 arr[k] = aux[j - l];
                 j++;
             }
         }
     }
 
+    /**
+     * 递归使用归并排序,对arr[l...r]的范围进行排序
+     *
+     * @param arr
+     * @param l
+     * @param r
+     */
     private static void sort(int[] arr, int l, int r) {
         if (l >= r)
             return;
